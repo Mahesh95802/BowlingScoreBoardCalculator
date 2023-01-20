4fac7a2757d9ceb7a9c6f45a8a2bd06255e6700b
Maheshwaran K <maheshwarankirupa@gmail.com>
2023-01-20 11:31:17 +0530

    feat: add new tests

--- scoreCalculator.test.js
+++ scoreCalculator.test.js
@@ -9,7 +9,19 @@ describe("Score Caculator", () => {
         expect(scoreCalculator([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).toBe(90)
     })
     it("should return correct output when given proper inputs", () => {
-        expect(scoreCalculator([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).toBe(90)
+        expect(scoreCalculator([6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])).toBe(16)
+    })
+    it("should return correct output when given proper inputs", () => {
+        expect(scoreCalculator([10, 9, 1, 10, 4, 5, 3, 6, 4, 4, 1, 3, 10, 1, 9, 5, 5, 2])).toBe(136)
+    })
+    it("should return correct output when given proper inputs", () => {
+        expect(scoreCalculator([10, 5, 5, 9, 0])).toBe(48)
+    })
+    it("should return correct output when given proper inputs", () => {
+        expect(scoreCalculator([10, 10, 10, 5, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])).toBe(81)
+    })
+    it("should return correct output when given proper inputs", () => {
+        expect(scoreCalculator([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 3, 10])).toBe(20)
     })
     it("Should throw error when not given array as input", () => {
         expect(() => scoreCalculator("abc")).toThrow(Error)
